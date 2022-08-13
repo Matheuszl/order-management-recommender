eeefd738adff38f877997d53119953bb4e85514c
Matheus <matheus.zzalamena@gmail.com>
2022-08-13 17:29:42 -0300

    Migration Mercado | Produto | Pedido

---
+++ modulo-web/database/migrations/2022_08_12_224937_create_pedidos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     * 
+     * 49:35 tags
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pedidos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('mercado_id')->constrained()->onDelete('cascade');
+            $table->string('descricao');
+            $table->enum('tipo', ['novo', 'troca'])->default('novo');
+            $table->string('slug')->unique();
+            $table->boolean('realizado')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pedidos');
+    }
+};
---
+++ modulo-web/database/migrations/2022_08_13_170500_create_pedido_produto_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pedido_produto', function (Blueprint $table) {
+            $table->foreignId('pedido_id')->constrained();
+            $table->foreignId('produto_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pedido_produto');
+    }
+};
