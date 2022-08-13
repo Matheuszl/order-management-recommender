18e05c4a9156b31a75edb6abf3f47329b815a0a2
Matheus <matheus.zzalamena@gmail.com>
2022-08-12 22:03:09 -0300

    Tradução do Sistema PT-BR

---
+++ modulo-web/lang/vendor/filament/en/login.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+
+    'title' => 'Login',
+
+    'heading' => 'Sign in to your account',
+
+    'buttons' => [
+
+        'submit' => [
+            'label' => 'Sign in',
+        ],
+
+    ],
+
+    'fields' => [
+
+        'email' => [
+            'label' => 'Email address',
+        ],
+
+        'password' => [
+            'label' => 'Password',
+        ],
+
+        'remember' => [
+            'label' => 'Lembrar Minha Senha',
+        ],
+
+    ],
+
+    'messages' => [
+        'failed' => 'These credentials do not match our records.',
+        'throttled' => 'Too many login attempts. Please try again in :seconds seconds.',
+    ],
+
+];
