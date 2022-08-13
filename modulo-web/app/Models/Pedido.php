e7612ecc01273d331d7dec373efdf2f5868156f4
Matheus <matheus.zzalamena@gmail.com>
2022-08-13 20:46:13 -0300

    Relacionamentos entre Models

--- modulo-web/app/Models/Pedido.php
+++ modulo-web/app/Models/Pedido.php
@@ -2,10 +2,36 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Mercado;
+use App\Models\Produto;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Pedido extends Model
 {
     use HasFactory;
+
+    protected $filllable = ['mercado_id', 'descricao', 'tipo', ''];
+
+    /**
+     * Os Pedidos tem apenas um mercado
+     * e 
+     * Um mercado pode estar em varios pedidos
+     */
+    public function mercado()
+    {
+        return $this->belongsTo(Mercado::class);
+    }
+
+
+    /**
+     * Um Produto pode estar presente em varios Pedidos
+     * E
+     * Um Pedido pode conter varios Produtos
+     * N*N
+     */
+    public function produtos()
+    {
+        return $this->belongsToMany(Produto::class);
+    }
 }
--- modulo-web/app/Models/Produto.php
+++ modulo-web/app/Models/Produto.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Pedido;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Produto extends Model
 {
     use HasFactory;
+
+    protected $filllable = ['nome', 'preco',  'slug', 'ativo', 'slug', 'realizado'];
+
+
+    public function produtos()
+    {
+        return $this->belongsToMany(Pedido::class);
+    }
+
 }
