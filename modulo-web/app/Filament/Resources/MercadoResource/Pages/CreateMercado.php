9da8e6a1c7cb2e97b2430ab6dd314b7e6af07746
Matheus <matheus.zzalamena@gmail.com>
2022-08-13 20:50:30 -0300

    Resources Templates Filament

---
+++ modulo-web/app/Filament/Resources/MercadoResource/Pages/CreateMercado.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\MercadoResource\Pages;
+
+use App\Filament\Resources\MercadoResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateMercado extends CreateRecord
+{
+    protected static string $resource = MercadoResource::class;
+}
